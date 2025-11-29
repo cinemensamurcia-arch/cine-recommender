037f6531e194721666979eb953ed125808ac6c43
cinemensamurcia-arch <cinemensamurcia@gmail.com>
2025-11-29 20:56:53 +0100

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,31 +1,13 @@
-// lib/firebaseAdmin.ts
+// lib/firestoreAdmin.ts
 import * as admin from "firebase-admin";
 
-let app: admin.app.App | null = null;
-
-export function initFirebaseAdmin() {
-  if (!app) {
-    const projectId = process.env.FIREBASE_PROJECT_ID;
-    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-
-    if (!projectId || !clientEmail || !privateKey) {
-      throw new Error("Faltan variables de entorno de Firebase Admin");
-    }
-
-    app = admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId,
-        clientEmail,
-        privateKey,
-      }),
-    });
-  }
-
-  return app;
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+    ),
+  });
 }
 
-export function getDb() {
-  if (!app) initFirebaseAdmin();
-  return admin.firestore();
-}
+export const db = admin.firestore();
+
