8ac929facb74b810ccf39370f6e35e465b52cdbd
cinemensamurcia-arch <cinemensamurcia@gmail.com>
2025-11-30 08:43:31 +0100

    Update recommendations.ts

--- api/recommendations.ts
+++ api/recommendations.ts
@@ -1,5 +1,5 @@
 // pages/api/recommendations.ts
-import type { NextApiRequest, NextApiResponse } from "next";
+
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const TMDB_API_KEY = process.env.TMDB_API_KEY; // o pon tu key fija si quieres
