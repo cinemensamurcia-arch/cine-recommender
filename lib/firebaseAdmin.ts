4c5503b1d4775edec4c0782c80a0c716d806d2bc
cinemensamurcia-arch <cinemensamurcia@gmail.com>
2025-11-30 07:58:49 +0100

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,13 +1,17 @@
-// lib/firestoreAdmin.ts
-import * as admin from "firebase-admin";
+// lib/firebaseAdmin.ts
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
-    ),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // Reemplaza \n escapados en la private key
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
 export const db = admin.firestore();
+
 
